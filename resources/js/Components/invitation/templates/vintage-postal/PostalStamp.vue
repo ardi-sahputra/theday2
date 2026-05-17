da0185065c8c33f35b581624b730a3c53fe1b816
Ardi Sahputra <167969030+ArdiSahputra@users.noreply.github.com>
2026-05-17 22:34:32 +0700

    feat(vintage-postal): scaffold 9 sub-component stubs

---
+++ resources/js/Components/invitation/templates/vintage-postal/PostalStamp.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({ todo: { type: Boolean, default: true } })
+</script>
+
+<template>
+    <div class="vp-stub" data-stub="PostalStamp"/>
+</template>
+
+<style scoped>
+.vp-stub { display: none; }
+</style>
