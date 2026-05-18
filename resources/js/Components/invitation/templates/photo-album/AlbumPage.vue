4fef3a127600e66dcf7e70c53d03310987f61eb4
Ardi Sahputra <167969030+ArdiSahputra@users.noreply.github.com>
2026-05-18 19:15:38 +0700

    feat(photo-album): scaffold 9 sub-component stubs

---
+++ resources/js/Components/invitation/templates/photo-album/AlbumPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({ todo: { type: Boolean, default: true } })
+</script>
+
+<template>
+    <div class="pa-stub" data-stub="AlbumPage"/>
+</template>
+
+<style scoped>
+.pa-stub { display: none; }
+</style>
---
+++ resources/js/Components/invitation/templates/photo-album/TheEndStamp.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({ todo: { type: Boolean, default: true } })
+</script>
+
+<template>
+    <div class="pa-stub" data-stub="TheEndStamp"/>
+</template>
+
+<style scoped>
+.pa-stub { display: none; }
+</style>
